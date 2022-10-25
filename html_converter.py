d9fd270f454b5febf390e01de1f0d067e2d8af7f
Guillermo Vizcaíno Román <guillevzn@gmail.com>
2022-10-25 21:19:32 +0200

    Update

--- html_converter.py
+++ html_converter.py
@@ -1,6 +1,6 @@
 import os
-os.chdir(r'C:\Users\guill\Documents\GitHub\informe-sevilla')
-#os.system('Jupyter nbconvert Informe_de_Coyuntura_del_Municipio_de_Sevilla.ipynb --to html --template classic --no-input --no-prompt')
+os.chdir(r'/Users/guille/Documents/GitHub/informe-sevilla')
+os.system('Jupyter nbconvert Informe_de_Coyuntura_del_Municipio_de_Sevilla.ipynb --to html --template classic --no-input --no-prompt')
 #os.system('Jupyter nbconvert Prolegomenos.ipynb --to html --template classic --no-input --no-prompt')
 
 #os.system('Jupyter nbconvert Informe_de_Coyuntura_del_Municipio_de_Sevilla.ipynb --to pdf --no-input --no-prompt')
